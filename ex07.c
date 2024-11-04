1c236a9df55de773a0205d87c20f828d94324ddc
LameeAlharkosh <jokerasamt2020@gmail.com>
2024-11-04 21:01:02 +0300

    Update ex07.c

--- ex07.c
+++ ex07.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+    for (int i = 1; i <= 11; i++)
+    {
+
+        int column1 = i * 5;
+        int column2 = 100 - ((i - 1) * 10 + 1);
+        int column3 = i;
+        printf("%3d  %3d  %3d\n", column1, column2, column3);
+    }
+}
 
